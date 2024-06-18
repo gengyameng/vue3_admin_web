608d9b73b36afd082d442ff6885625619264165e
Eirene Jackson <33662520+gengyameng@users.noreply.github.com>
2024-06-18 10:16:14 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    <h1>App根组件</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = "我爱你祖国";
+</script>
+
+<style scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: skyblue;
+}
+</style>
