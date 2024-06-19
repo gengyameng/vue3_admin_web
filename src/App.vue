8c6d3411216d1334d1764cc8cac31fdcbaaef07f
Eirene Jackson <33662520+gengyameng@users.noreply.github.com>
2024-06-19 09:47:39 +0800

    环境配置

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="box">
-    <h1>App根组件</h1>
+    <h1>Svg测试</h1>
+    <!-- 测试svg图表使用 -->
+    <!-- svg:图表外层容器，内部需要与use标签结合使用 -->
+    <svg>
+      <!-- xlink:href指定用哪一个图标 属性值使用#icon-图标名字 -->
+      <use xlink:href="#icon-pay" fill="yellow"></use>
+    </svg>   
   </div>
 </template>
 
 <script setup lang="ts">
-const str = "我爱你祖国";
+
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from "vue";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from "@/App.vue";
 
-import App from "./App.vue";
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).mount("#app");
+// console.log(import.meta.env);
+
+const app = createApp(App)
+
+// 安装element-plus插件
+app.use(ElementPlus, {
+  locale: zhCn
+})
+
+// svg插件需要配置代码
+import 'virtual:svg-icons-register'
+app.mount("#app");
