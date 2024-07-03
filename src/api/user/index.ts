05b518d6b1064bc15fd2e3d18e373fa3216e1577
Eirene Jackson <33662520+gengyameng@users.noreply.github.com>
2024-07-04 07:43:16 +0800

    user api

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,12 +1,16 @@
 // 用户模块接口统一管理
 import request from "@/utils/service/index";
-import { loginFormData, loginResponseData, userInfoResponseData } from "@/api/user/type";
+import {
+  loginFormData,
+  loginResponseData,
+  userInfoResponseData,
+} from "@/api/user/type";
 
 // 项目相关的请求地址
 enum API {
   LOGIN_URL = "/admin/acl/index/login",
   USERINFO_URL = "/admin/acl/index/info",
-  LOGOUT_URL = '/admin/acl/index/logout'
+  LOGOUT_URL = "/admin/acl/index/logout",
 }
 
 // 登录接口
@@ -21,5 +25,5 @@ export const reqUserInfo = () => {
 
 // 退出登录接口
 export const reqLogout = () => {
-  return request.post<any, any>(API.LOGOUT_URL)
-}
+  return request.post<any, any>(API.LOGOUT_URL);
+};
--- src/api/user/type.ts
+++ src/api/user/type.ts
@@ -2,20 +2,20 @@
 
 // 用户登录请求参数的ts类型
 export interface loginFormData {
-  username: string
-  password: string
+  username: string;
+  password: string;
 }
 
 // 定义全部接口返回数据都拥有的ts类型
 export interface ResponseData {
   code: number;
   message: string;
-  ok: boolean
+  ok: boolean;
 }
 
 // 定义登录接口返回数据类型
 export interface loginResponseData extends ResponseData {
-  data: string
+  data: string;
 }
 
 // 用户信息类型
@@ -29,6 +29,5 @@ export interface userInfoType {
 
 // 定义获取用户信息返回数据类型
 export interface userInfoResponseData extends ResponseData {
-  data: userInfoType
+  data: userInfoType;
 }
-
